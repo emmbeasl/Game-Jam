2291b1d06c7dc1a61ce32b9eebc2189446741216
brybalti <brybalti@iu.edu>
2020-12-02 18:43:39 -0500

    added hud

---
+++ Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var score = 0
+var lives = 5
+var level = 1
+var time = 0
+
+
+func _ready():
+	pass
---
+++ HUD/HUD.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _physics_process(delta):
+	$Score.text = "Score: " + str(Global.score)
+
+func _ready():
+	pass
